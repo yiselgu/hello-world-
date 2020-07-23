b2482154539c1da74f70525b93c71cc90b6b65af
yisel <yiselgu@gmail.com>
2020-07-23 17:57:40 -0500

    cuenta bancaria

---
+++ js/objetos.js
@@ -0,0 +1,37 @@
+var CuentaAhorro = 
+        {
+            titular: "Alex",
+            saldo : 0,
+            ingresar: function(cantidad)
+            {
+                console.log("Has ingresado" + cantidad + "Euros");
+                this.saldo += cantidad;
+            },
+
+            extraer: function(cantidad)
+            {
+                console.log("Has extraido" + cantidad + "Euros");
+                return this.saldo -= cantidad;
+            },
+
+            informar: function(){
+                return "Nombre del Titular:" + this.titular + "El Saldo de la Cuenta es \n" + this.saldo;
+            },
+            
+        } 
+
+console.log(CuentaAhorro.titular);
+console.log("------------------");
+console.log(CuentaAhorro.ingresar(80));
+console.log(CuentaAhorro.extraer(20));
+
+console.log(CuentaAhorro.informar());
+
+
+
+
+
+
+
+
+
